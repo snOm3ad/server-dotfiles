30ae764825c2d1be523cc694444aa45cd1ae5910
Oliver T <geronimooliver00@gmail.com>
2023-11-16 11:11:21 -0500

    fixed bugs in setup script

--- README.md
+++ README.md
@@ -11,10 +11,14 @@ It also contains a `nvim` folder which you can directly copy into `mkdir ~/.conf
 
 
 ## New Guide for Fresh Installations in VMs
-The first thing you need to do is set some required environment variables `GHTOKEN` is used to fetch the lua neovim config files from the private repository.
+The first thing you need to do is upgrade your apt packages as well as upgrade your kernel:
+```bash
+sudo apt update && sudo apt upgrade
+```
 
-Once this is done run `chmod +x setup.sh && ./setup.sh` and wait for everything to get setup.
+Once this runs you'll get a message saying that the kernel patch version should be upgraded and that some services need to be restarted. By default it will restart all services that would allow you to keep the connection open. So in order to remove the leftover services run `sudo reboot`, NOTE that you will be logged out immediately.
 
+After the reboot you can start running the `setup.sh` file, by running `chmod +x setup.sh && ./setup.sh`. You'll need to set the `GHTOKEN` **before** which as it is used to fetch the lua neovim config files from the private repository.
 
 ## Guide for New Installations in VMs (Deprecated)
 First install `nvim`
--- setup.sh
+++ setup.sh
@@ -11,10 +11,6 @@ fail() {
     echo -e "[ERROR] $1" && exit -1
 }
 
-update() {
-    sudo apt update && sudo apt upgrade
-}
-
 install() {
     print "INSTALLING $1"
     sudo apt install "$1" || fail "Could not install $1"
@@ -30,27 +26,91 @@ clone_private_repo() {
     git clone "https://oauth2:$GHTOKEN@github.com/snOm3ad/dot-nvim-lua" .
 }
 
-print "UPDATING PACKAGES"
-update || fail "Could not update apt packages."
+neovim_beta() {
+    print "INSTALLING $1"
+    sudo snap install --beta "$1" --classic || fail "Could not install $1"
+}
 
-install "node"
-install "fd"
+llvm_setup() {
+    # get ubuntu codename
+    CODENAME="$(lsb_release -c | tail | awk '{ print $2 }' )"
+    [ -z "$CODENAME" ] && fail "Could not fetch codename for this distro"
+
+    # get the gpg key
+    curl -O -L 'https://apt.llvm.org/llvm-snapshot.gpg.key' && 
+        sudo apt-key add 'llvm-snapshot.gpg.key' && 
+        rm 'llvm-snapshot.gpg.key' ||
+        fail "Could not add gpg key"
+
+    # add repository
+    add-apt-repository "deb http://apt.llvm.org/$CODENAME/ llvm-toolchain-$CODENAME main"
+}
+
+python3_venv() {
+    # check if python3 is installed.
+    [ -z "$(which python3)" ] && fail "python3 is required, however it was not found"
+    # make .venv dir
+    mkdir -p "$HOME/.venvs/" && cd "$HOME/.venvs/"
+    
+    # get python3 version
+    VERSION="$(python3 --version | python3 -c 'import sys; v = [line.split() for line in sys.stdin]; _, v = v[0]; print(".".join(v.split(".")[:-1]))' )"
+    [ -n "$VERSION" ] && 
+        install "python$PYTHON_VERSION-venv" &&
+        python3 -m venv neovim || 
+        fail "Could not create python3 virtual environment"
+}
+
+print "INSTALLING MISC PACKAGES"
+install "nodejs"
+install "fd-find"
 install "ripgrep"
 install "fzf"
-install "neovim"
 install "git-delta"
 install "tmux"
-install "lua"
-install "geckodriver"
-install "pyright"
+install "snapd"
+install "ppa-purge"
 
-# install tmux-dev
+print "INSTALLING LLVM"
+llvm_setup && 
+    install "lld" &&
+    install "lldb" &&
+    install "llvm" && 
+    install "llvm-dev" &&
+    install "llvm-runtime" &&
+
+print "INSTALLING CLANG SUITE"
+install "clang" && 
+    install "clangd" &&
+    install "clang-format" && 
+    install "clang-tidy" &&
+    install "clang-tools" &&
+    install "python3-clang"
+
+print "INSTALLING C-LIBS"
+install "libclang-rt-dev" &&
+    install "libc++-dev" &&
+    install "libc++abi-dev" &&
+    install "libunwind-dev"
+
+print "INSTALLING WASM STUFF"
+install "libclang-rt-dev-wasm32" &&
+    install "libclang-rt-dev-wasm64" &&
+    install "libc++-dev-wasm32" &&
+    install "libc++abi-dev-wasm32" &&
+    install "libclang-rt-dev-wasm32" &&
+    install "libclang-rt-dev-wasm64"
+
+neovim_beta "neovim"
+
 print "INSTALLING TMUX-DEV SCRIPT"
 scripts
 
-# install dot files
 print "INSTALLING DOT FILES"
 mv .bash* .tmux.conf "$HOME/"
+
+# required for neovim
+print "INSTALLING PYTHON VENV"
+python3_venv
 
 print "INSTALLING NEOVIM CONFIG"
 mkdir -p "$HOME/.config/nvim" &&
@@ -58,6 +118,5 @@ mkdir -p "$HOME/.config/nvim" &&
     clone_private_repo ||
     fail "Could not install dot files"
 
-# finally install rust
 print "INSTALLING RUST"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh || fail "Could not install rust!"
